6c03d7edb9ebac3003761838a2477e848089ec6d
Fifoll <kakolfilip@wp.pl>
2024-05-11 15:08:47 +0200

    add login feature

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import register from '../controllers/user/register.js';
+import login from '../controllers/user/login.js';
 
 const userRoutes = express.Router();
 
 userRoutes.post('/register', register);
+
+userRoutes.post('/login', login);
 
 
 export default userRoutes;
